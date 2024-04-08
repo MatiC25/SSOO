117dc4b3b70ba49313e02f2fa713c4466e6187a8
Federico Encinaz Sayago <fencinazsayago@frba.utn.edu.ar>
2024-04-08 16:01:18 -0300

    Upgrade: Se agrega funciones de sockets :)

--- utils/src/utils/socket.c
+++ utils/src/utils/socket.c
@@ -1,52 +1,139 @@
 #include "socket.h"
 
-
-//usuario
-void* serializar_paquete(t_paquete* paquete, int bytes)
+// usuario
+void *serializar_paquete(t_paquete *paquete, int bytes)
 {
-	void * magic = malloc(bytes);
+	void *magic = malloc(bytes);
 	int desplazamiento = 0;
 
 	memcpy(magic + desplazamiento, &(paquete->codigo_operacion), sizeof(int));
-	desplazamiento+= sizeof(int);
+	desplazamiento += sizeof(int);
 	memcpy(magic + desplazamiento, &(paquete->buffer->size), sizeof(int));
-	desplazamiento+= sizeof(int);
+	desplazamiento += sizeof(int);
 	memcpy(magic + desplazamiento, paquete->buffer->stream, paquete->buffer->size);
-	desplazamiento+= paquete->buffer->size;
+	desplazamiento += paquete->buffer->size;
 
 	return magic;
 }
 
-int crear_conexion(char *ip, char* puerto)
+// INICIA SERVER ESCUCHANDO EN IP:PUERTO
+int iniciar_servidor(t_log *logger, const char *name, char *ip, char *puerto)
 {
-	struct addrinfo hints;
-	struct addrinfo *server_info;
+	int socket_servidor;
+	struct addrinfo hints, *servinfo;
 
+	// Inicializando hints
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
-	getaddrinfo(ip, puerto, &hints, &server_info);
+	// Recibe los addrinfo
+	getaddrinfo(ip, puerto, &hints, &servinfo);
 
-	// Ahora vamos a crear el socket.
-	int socket_cliente = 0;
+	bool conecto = false;
 
-	// Ahora que tenemos el socket, vamos a conectarlo
-	socket_cliente = socket(server_info->ai_family,
-                         	server_info->ai_socktype,
-                         	server_info->ai_protocol);
+	// Itera por cada addrinfo devuelto
+	for (struct addrinfo *p = servinfo; p != NULL; p = p->ai_next)
+	{
+		socket_servidor = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+		if (socket_servidor == -1) // fallo de crear socket
+			continue;
 
+		if (bind(socket_servidor, p->ai_addr, p->ai_addrlen) == -1)
+		{
+			// Si entra aca fallo el bind
+			close(socket_servidor);
+			continue;
+		}
+		// Ni bien conecta uno nos vamos del for
+		conecto = true;
+		break;
+	}
 
-	connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen);
-	freeaddrinfo(server_info);
+	if (!conecto)
+	{
+		free(servinfo);
+		return 0;
+	}
+
+	listen(socket_servidor, SOMAXCONN); // Escuchando (hasta SOMAXCONN conexiones simultaneas)
+
+	// Aviso al logger
+	log_info(logger, "Escuchando en %s:%s (%s)\n", ip, puerto, name);
+
+	freeaddrinfo(servinfo);
+
+	return socket_servidor;
+}
+
+// ESPERAR CONEXION DE CLIENTE EN UN SERVER ABIERTO
+int esperar_cliente(t_log *logger, const char *name, int socket_servidor)
+{
+	struct sockaddr_in dir_cliente;
+	socklen_t tam_direccion = sizeof(struct sockaddr_in);
+
+	int socket_cliente = accept(socket_servidor, (void *)&dir_cliente, &tam_direccion);
+
+	log_info(logger, "Cliente conectado (a %s)\n", name);
 
 	return socket_cliente;
 }
 
-void enviar_mensaje(char* mensaje, int socket_cliente)
+// CLIENTE SE INTENTA CONECTAR A SERVER ESCUCHANDO EN IP:PUERTO
+int crear_conexion(t_log *logger, const char *server_name, char *ip, char *puerto)
 {
-	t_paquete* paquete = malloc(sizeof(t_paquete));
+
+	struct addrinfo hints, *servinfo;
+
+	// Init de hints
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	// Recibe addrinfo
+	getaddrinfo(ip, puerto, &hints, &servinfo);
+
+	// Crea un socket con la informacion recibida (del primero, suficiente)
+	int socket_cliente = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+
+	printf("Socket cliente: %d\n", socket_cliente);
+
+	// Fallo en crear el socket
+	if (socket_cliente == -1)
+	{
+		log_error(logger, "Error creando el socket para %s:%s", ip, puerto);
+		return 0;
+	}
+
+	// Error conectando
+	if (connect(socket_cliente, servinfo->ai_addr, servinfo->ai_addrlen) == -1)
+	{
+		log_error(logger, "Error al conectar (a %s)\n", server_name);
+		freeaddrinfo(servinfo);
+		return 0;
+	}
+	else
+		log_info(logger, "Cliente conectado en %s:%s (a %s)\n", ip, puerto, server_name);
+
+	freeaddrinfo(servinfo);
+
+	return socket_cliente;
+}
+
+// Crear funcion que permita crear una conexion con un timer
+
+// CERRAR CONEXION
+void liberar_conexion(int *socket_cliente)
+{
+	close(*socket_cliente);
+	*socket_cliente = -1;
+}
+
+void enviar_mensaje(char *mensaje, int socket_cliente)
+{
+	t_paquete *paquete = malloc(sizeof(t_paquete));
 
 	paquete->codigo_operacion = MENSAJE;
 	paquete->buffer = malloc(sizeof(t_buffer));
@@ -54,35 +141,32 @@ void enviar_mensaje(char* mensaje, int socket_cliente)
 	paquete->buffer->stream = malloc(paquete->buffer->size);
 	memcpy(paquete->buffer->stream, mensaje, paquete->buffer->size);
 
-	int bytes = paquete->buffer->size + 2*sizeof(int);
+	int bytes = paquete->buffer->size + 2 * sizeof(int);
 
-	void* a_enviar = serializar_paquete(paquete, bytes);
+	void *a_enviar = serializar_paquete(paquete, bytes);
 
 	send(socket_cliente, a_enviar, bytes, 0);
-
-	
 
 	free(a_enviar);
 	eliminar_paquete(paquete);
 }
 
-
-void crear_buffer(t_paquete* paquete)
+void crear_buffer(t_paquete *paquete)
 {
 	paquete->buffer = malloc(sizeof(t_buffer));
 	paquete->buffer->size = 0;
 	paquete->buffer->stream = NULL;
 }
 
-t_paquete* crear_paquete(void)
+t_paquete *crear_paquete(void)
 {
-	t_paquete* paquete = malloc(sizeof(t_paquete));
+	t_paquete *paquete = malloc(sizeof(t_paquete));
 	paquete->codigo_operacion = PAQUETE;
 	crear_buffer(paquete);
 	return paquete;
 }
 
-void agregar_a_paquete(t_paquete* paquete, void* valor, int tamanio)
+void agregar_a_paquete(t_paquete *paquete, void *valor, int tamanio)
 {
 	paquete->buffer->stream = realloc(paquete->buffer->stream, paquete->buffer->size + tamanio + sizeof(int));
 
@@ -92,17 +176,17 @@ void agregar_a_paquete(t_paquete* paquete, void* valor, int tamanio)
 	paquete->buffer->size += tamanio + sizeof(int);
 }
 
-void enviar_paquete(t_paquete* paquete, int socket_cliente)
+void enviar_paquete(t_paquete *paquete, int socket_cliente)
 {
-	int bytes = paquete->buffer->size + 2*sizeof(int);
-	void* a_enviar = serializar_paquete(paquete, bytes);
+	int bytes = paquete->buffer->size + 2 * sizeof(int);
+	void *a_enviar = serializar_paquete(paquete, bytes);
 
 	send(socket_cliente, a_enviar, bytes, 0);
-	
+
 	free(a_enviar);
 }
 
-void eliminar_paquete(t_paquete* paquete)
+void eliminar_paquete(t_paquete *paquete)
 {
 	free(paquete->buffer->stream);
 	free(paquete->buffer);
@@ -114,56 +198,10 @@ void liberar_conexion(int socket_cliente)
 	close(socket_cliente);
 }
 
-
-//servidor
-t_log* logger;
-
-int iniciar_servidor(char* IP, char* PUERTO)
-{
-	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-
-	struct addrinfo hints, *servinfo, *p;
-
-	memset(&hints, 0, sizeof(hints)); 
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
-	
-	getaddrinfo(IP, PUERTO, &hints, &servinfo); //datos del servidor pre establecido
-
-	//Averiguar como hacer para pasar IP y PUERTO  de usuario a servidor..???
-
-	// Creamos el socket de escucha del servidor
-	int socket_servidor = socket(servinfo->ai_family,
-                				servinfo->ai_socktype,
-                				servinfo->ai_protocol);
-	// Asociamos el socket a un puerto
-	bind(socket_servidor, servinfo->ai_addr, servinfo->ai_addrlen);
-	// Escuchamos las conexiones entrantes
-	listen(socket_servidor, SOMAXCONN);
-	freeaddrinfo(servinfo);
-	log_trace(logger, "Listo para escuchar a mi cliente"); 
-
-	return socket_servidor;
-}
-
-int esperar_cliente(int socket_servidor)
-{
-	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	
-
-	// Aceptamos un nuevo cliente
-	int socket_cliente = accept(socket_servidor, NULL, NULL);
-	
-	log_info(logger, "Se conecto un cliente!");
-
-	return socket_cliente;
-}
-
 int recibir_operacion(int socket_cliente)
 {
 	int cod_op;
-	if(recv(socket_cliente, &cod_op, sizeof(int), MSG_WAITALL) > 0)
+	if (recv(socket_cliente, &cod_op, sizeof(int), MSG_WAITALL) > 0)
 		return cod_op;
 	else
 	{
@@ -172,9 +210,9 @@ int recibir_operacion(int socket_cliente)
 	}
 }
 
-void* recibir_buffer(int* size, int socket_cliente)
+void *recibir_buffer(int *size, int socket_cliente)
 {
-	void * buffer;
+	void *buffer;
 
 	recv(socket_cliente, size, sizeof(int), MSG_WAITALL);
 	buffer = malloc(*size);
@@ -186,27 +224,27 @@ void* recibir_buffer(int* size, int socket_cliente)
 void recibir_mensaje(int socket_cliente)
 {
 	int size;
-	char* buffer = recibir_buffer(&size, socket_cliente);
+	char *buffer = recibir_buffer(&size, socket_cliente);
 	log_info(logger, "Me llego el mensaje %s", buffer);
 	free(buffer);
 }
 
-t_list* recibir_paquete(int socket_cliente)
+t_list *recibir_paquete(int socket_cliente)
 {
 	int size;
 	int desplazamiento = 0;
-	void * buffer;
-	t_list* valores = list_create();
+	void *buffer;
+	t_list *valores = list_create();
 	int tamanio;
 
 	buffer = recibir_buffer(&size, socket_cliente);
-	while(desplazamiento < size)
+	while (desplazamiento < size)
 	{
 		memcpy(&tamanio, buffer + desplazamiento, sizeof(int));
-		desplazamiento+=sizeof(int);
-		char* valor = malloc(tamanio);
-		memcpy(valor, buffer+desplazamiento, tamanio);
-		desplazamiento+=tamanio;
+		desplazamiento += sizeof(int);
+		char *valor = malloc(tamanio);
+		memcpy(valor, buffer + desplazamiento, tamanio);
+		desplazamiento += tamanio;
 		list_add(valores, valor);
 	}
 	free(buffer);
