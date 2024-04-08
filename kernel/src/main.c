72f0ee986ae85c82447e7e44434912d8d96474e6
NicoferUtn <nfernandezruoff@frba.utn.edu.ar>
2024-04-08 19:27:50 +0000

    Update: errores de federico :)

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -9,7 +9,6 @@ void inicializar_config(void)
     config_kernel = malloc(sizeof(t_config_k));
     config_kernel->ip_memoria = NULL;
     config_kernel->ip_cpu = NULL;
-    config_kernel->ip_fs = NULL;
     config_kernel->algoritmo_planificacion = NULL;
     config_kernel->inst_recursos = NULL;
     config_kernel->recursos = NULL;
@@ -20,15 +19,18 @@ int main()
     t_log *logger_kernel = log_create("kernel.log", "KERNEL", true, LOG_LEVEL_INFO);
     inicializar_config(); // Inicializo la variable global config_kernel! -> No se si es la mejor forma de hacerlo!
 
+
+
+    printf("hola");
     // NEW = crear_cola() // Hay que armar la PCB!
     // READY = crear_cola()
     // EXEC = crear_cola()
     // BLOCK = crear_cola()
     // EXIT = crear_cola()
 
-    int md_memoria = 0, md_cpu_dt = 0, md_cpu_it = 0, md_fs = 0;
+    int md_memoria = 0, md_cpu_dt = 0, md_cpu_it = 0;
 
-    if (!cargar_configuraciones(config_kernel, logger_kernel) || !generar_conexiones(logger_kernel, config_kernel, &md_memoria, &md_cpu_dt, &md_cpu_it, &md_fs))
+    if (!cargar_configuraciones(config_kernel, logger_kernel) || !generar_conexiones(logger_kernel, config_kernel, &md_memoria, &md_cpu_dt, &md_cpu_it))
     { // Generar conexiones, no va a mantener la conexion, sino que va a crear la conexion y la va a cerrar!
         log_error(logger_kernel, "No se pudieron generar las conexiones");
 
@@ -39,6 +41,6 @@ int main()
     // iniciar_modulo(); // Funcion en proceso de creacion!
 
     cerrar_programa(logger_kernel);
-
+    printf("hola");
     return 0;
 }
